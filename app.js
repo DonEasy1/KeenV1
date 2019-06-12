ed37b5c407fd7d9086d091de5647616a823f528d
Dawn Ann <doneasy1@gmail.com>
2019-06-12 18:46:11 +0000

    added 'new campground form', updated routing

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+/* global name */
+/* global image */
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+var campgrounds = [
+        {name: 'Salmon Creek', image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg'},
+        {name: 'Granite Hill', image: 'https://farm9.staticflickr.com/8422/7842069486_c61e4c6025.jpg'},
+        {name: 'Little Popo Aggie', image: 'https://farm9.staticflickr.com/8041/7930201874_6c17ed670a.jpg'}
+    ];
+    
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
@@ -7,12 +18,21 @@ app.get('/', (req, res) => {
 });
 
 app.get('/campgrounds', (req, res) => {
-    var campgrounds = [
-        {name: 'Salmon Creek', image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg'},
-        {name: 'Granite Hill', image: 'https://farm9.staticflickr.com/8422/7842069486_c61e4c6025.jpg'},
-        {name: 'Little Popo Aggie', image: 'https://farm9.staticflickr.com/8041/7930201874_6c17ed670a.jpg'}
-    ];
     res.render('campgrounds',{campgrounds: campgrounds});
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('new.ejs')
+});
+
+app.post('/campgrounds', (req,res) => {
+    var name = req.body.name;
+    var image = req.body.image;
+    //get data from form and add to campgrounds array
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    //recirect back to campgrounds page
+    res.redirect('/campgrounds');
 });
 
 app.listen(process.env.PORT, process.env.IP, () => {
