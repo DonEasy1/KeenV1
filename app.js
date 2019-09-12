c99fa53a64702a9707b07aaadbd859a67ebdc5d4
Dawn Beattie <serenity.care.mc@gmail.com>
2019-09-12 04:01:10 +0000

    listening PORT change

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log('KeenKamps');
 });
